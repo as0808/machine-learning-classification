5fb1544830e469df82a805bf80ec8a67d8fdfbc3
as0808 <67689138+as0808@users.noreply.github.com>
2021-10-05 14:38:33 +0530

    Create code.py

---
+++ red-wine-quality/code.py
@@ -0,0 +1,50 @@
+import pandas as pd
+import numpy as np
+
+rw=pd.read_csv("/winequality-red.csv")
+rw
+
+x=rw.iloc[:,:-1].values
+x
+
+y=rw.iloc[:,-1].values
+y
+
+x=pd.DataFrame(x)
+x
+
+y=pd.DataFrame(y)
+y
+
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test=train_test_split(x, y, test_size=0.2, random_state=1)
+
+#RandomForestClassifier
+from sklearn.ensemble import RandomForestClassifier
+model=RandomForestClassifier()
+
+model.fit(x_train, y_train)
+
+model.predict(x_test)
+
+model.score(x_test, y_test)
+
+#DecisionTreeClassifier
+from sklearn.tree import DecisionTreeClassifier
+model1=DecisionTreeClassifier()
+
+model1.fit(x_train, y_train)
+
+model1.predict(x_test)
+
+model1.score(x_test, y_test)
+
+#KNeighborsClassifier
+from sklearn.neighbors import KNeighborsClassifier
+model2=KNeighborsClassifier()
+
+model2.fit(x_train, y_train)
+
+model2.predict(x_test)
+
+model2.score(x_test, y_test)
